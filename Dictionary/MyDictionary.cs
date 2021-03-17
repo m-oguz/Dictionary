7a277ded059622fbcd95d1e158ff698a0e952254
mehmet oğuz <oguzmehm@hotmail.com>
2021-03-17 23:51:09 +0200

    Add project files.

---
+++ Dictionary/MyDictionary.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dictionary
+{
+    class MyDictionary<MyKey,MyValue>
+    {
+        
+        MyKey[] keys;
+        MyValue[] values;
+
+        MyKey[] tempKeys;
+        MyValue[] tempValues;
+
+
+        public MyDictionary()
+        {
+            keys = new MyKey[0];
+            values = new MyValue[0];
+        }
+
+        public void Add(MyKey theKey, MyValue theValue)
+        { 
+            
+            tempKeys = keys;
+            tempValues = values;
+            
+            
+            keys = new MyKey[tempKeys.Length+1];
+            values = new MyValue[tempValues.Length+1];
+
+            for (int i = 0; i<tempKeys.Length;i++)
+            {
+                 keys[i] = tempKeys[i];
+                 values[i] = tempValues[i];
+            }
+            keys[keys.Length-1] = theKey;
+            values[values.Length-1] = theValue;
+        }
+        public void listTheKeys()
+        {
+            Console.WriteLine("The words that you can translate to Finnish:");
+            Console.WriteLine("-----------------------------------------------");
+            foreach (var item in keys)
+            {
+                Console.WriteLine(item);
+            }
+        }
+        
+        public MyValue Get(MyKey key)
+        {
+            int j=keys.Length +1 ;
+           for(int i=0; i < keys.Length; i++)
+            {
+                if(key.Equals(keys[i]))
+                {
+                    j = i;
+                }
+            }
+            return values[j];
+        }
+    }
+}
---
+++ Dictionary/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+
+namespace Dictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            MyDictionary<string, string> sozluk = new MyDictionary<string, string>();
+            sozluk.Add("key", "avain");
+            sozluk.Add("door", "ovi");
+            sozluk.Add("wall", "seinä");
+            sozluk.Add("book", "kirja");
+            sozluk.Add("computer", "tietokone");
+            sozluk.Add("home", "koti");
+            sozluk.Add("phone", "puhelin");
+            sozluk.Add("bed", "sänky");
+            sozluk.Add("info", "tieto");
+            sozluk.Add("school", "koulu");
+            sozluk.Add("hand", "käsi");
+            sozluk.Add("car", "auto");
+            sozluk.Add("carpet", "matto");
+            sozluk.Add("water", "vesi");
+            sozluk.Add("room", "huone");
+
+
+
+            sozluk.listTheKeys();
+
+            while (true)
+            {
+                Console.WriteLine("Type the word to translate");
+
+
+                string input = Console.ReadLine();
+                Console.WriteLine("\""+ input + "\" means \"" + sozluk.Get(input) + "\"."); 
+            }
+
+        }
+    }
+}
